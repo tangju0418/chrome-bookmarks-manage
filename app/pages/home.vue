324febb1d2a74867646378961ddc07adfa5ea3c9
唐菊 <635465547@qq.com>
2017-04-12 22:05:03 +0800

    vue打包生成页面放入扩展文件夹的测试

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
+    <div class="weui-form-preview">
+            <div class="weui-form-preview__hd">
+                <label class="weui-form-preview__label">付款金额</label>
+                <em class="weui-form-preview__value">¥2400.00</em>
+            </div>
+            <div class="weui-form-preview__bd" v-for="item in Items">
+                <div class="weui-form-preview__item" v-for=" i in item">
+                    <label class="weui-form-preview__label">商品{{i.length}}</label>
+                    <span class="weui-form-preview__value">电动打蛋机</span>
+                </div>
 
+            </div>
+
+        </div>
   </div>
 </template>
 
@@ -9,15 +22,18 @@ import Vue from 'vue'
 
 
 export default {
-  route: {
-    data: function(transition) {
-
-    }
+ beforeRouteEnter(to, from, next){
+    next(vm =>{
+      chrome.bookmarks.getTree(
+        function(bookmarkTreeNodes) {
+          console.log('书签列表0：',bookmarkTreeNodes)
+          vm.Items = bookmarkTreeNodes
+        });
+      })
   },
-
   data: function() {
     return {
-
+      Items:[]
     }
   },
 
@@ -32,6 +48,6 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-@import "styl/publics";
+  @import "styl/publics";
 
 </style>

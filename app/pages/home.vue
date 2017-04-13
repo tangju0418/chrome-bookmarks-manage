1cae1b7d85215de70dc4e6b2a69d2572d80816be
唐菊 <635465547@qq.com>
2017-04-13 20:42:42 +0800

    页面样式

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -14,6 +14,30 @@
       </div>
 
     </div>
+    <div class="weui-form-preview">
+      <div class="weui-form-preview__hd" style="background-color:#ccc">
+          <label class="weui-form-preview__label">书签</label>
+          <a class="weui-btn weui-btn_plain-primary wran">删除</a>
+          <a class="weui-btn weui-btn_plain-primary">修改</a>
+          <a class="weui-btn weui-btn weui-btn_plain-default">添加</a>
+      </div>
+      <div v-for=" i in 2">
+        <div class="weui-form-preview__hd" >
+          <label class="weui-form-preview__label" style="text-indent:2rem">常用</label>
+          <a class="weui-btn weui-btn_plain-primary wran">删除</a>
+          <a class="weui-btn weui-btn_plain-primary">修改</a>
+          <a class="weui-btn weui-btn weui-btn_plain-default">添加</a>
+        </div>
+        <div class="weui-form-preview__bd"  v-for=" j in 2">
+            <div class="weui-form-preview__item">
+                <label class="weui-form-preview__label" style="text-indent:2rem">百度</label>
+                <a class="weui-btn weui-btn_plain-primary wran">删除</a>
+                <a class="weui-btn weui-btn_plain-primary">修改</a>
+            </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -49,6 +73,33 @@ export default {
 
 <style lang="stylus" scoped>
   @import "styl/publics";
+  .weui-btn
+    width 100px
+    font-size 14px
+    float right
+    margin-left 10px
+  .wran
+    border 1px solid #E64340
+    color #E64340
+    &:active
+      border 1px solid #e48987
+      color #e48987
+  .weui-btn+.weui-btn
+    margin-top 0
+
   .weui-form-preview__hd
     background-color #f3f3f3
+    height 30px
+    .weui-btn
+      line-height 30px
+
+  .weui-form-preview__bd
+    border-bottom 1px solid #f3f3f3
+    padding 5px 15px
+    height 30px
+    .weui-form-preview__item
+      margin-left 15px
+    .weui-btn
+      line-height 30px
+
 </style>
